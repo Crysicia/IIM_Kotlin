d00fd1b031fa7e06cfdceb22a6e8595183fe137e
Crysicia <7459685+Crysicia@users.noreply.github.com>
2019-12-18 20:56:01 +0100

    Add aniaml selection

--- animalerie/app.kt
+++ animalerie/app.kt
@@ -1,7 +1,23 @@
+val selectableAnimals = mapOf<String, Animal>("chat" to Chat(), "chien" to Chien(), "perroquet" to Perroquet())
+
+fun select(): Animal {
+    val animals = selectableAnimals.keys
+    print("Select an animal between the followings : $animals\n> ")
+
+    val input = readLine()!!.toLowerCase()
+
+    return if (animals.contains(input)) {
+        selectableAnimals[input]!!
+    } else {
+        println("Sorry I don't know this kind of animal")
+        select()
+    }
+}
+
 fun main(): Unit {
-    val animalerie: Animalerie = Animalerie()
-    animalerie.add(Chat())
-    animalerie.add(Chien())
-    animalerie.add(Perroquet())
+    val animalerie = Animalerie()
+    animalerie.add(select())
+    animalerie.add(select())
+    animalerie.add(select())
     animalerie.affiche()
 }
