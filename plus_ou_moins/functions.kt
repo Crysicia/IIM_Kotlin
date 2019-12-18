cdcfa707f4feadc9e253c73b36eccffa01f6e842
Crysicia <7459685+Crysicia@users.noreply.github.com>
2019-12-18 15:24:26 +0100

    Clean plus_ou_moins

--- plus_ou_moins/functions.kt
+++ plus_ou_moins/functions.kt
@@ -5,6 +5,7 @@ fun readPassword(): CharArray = System.console().readPassword()
 fun parsePassword(): Int {
     print("Enter a number to guess : ")
     var input: String = String(readPassword())
+
     return if (input.toIntOrNull() == null) {
         println("Please enter an integer")
         parsePassword()
